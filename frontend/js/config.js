00499579da9a7519d46276e9977dfb0b80a1a6bd
Saqr <saqr41447@gmail.com>
2026-07-07 16:12:23 +0300

    Configure project for deployment

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:8000/api";
+export const API_URL = "https://saqr.pythonanywhere.com/api";
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
